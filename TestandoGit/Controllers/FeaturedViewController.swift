6712237d34b93fcdaf5b59347d78675e4f52a399
SofiRibeiro <sofiasantana168@gmail.com>
2022-07-12 18:03:12 -0300

    Organiza pastas do arquivo e funções

--- TestandoGit/Controllers/FeaturedViewController.swift
+++ TestandoGit/Controllers/FeaturedViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class FeaturedViewController: UIViewController, UICollectionViewDataSource {
+class FeaturedViewController: UIViewController {
     
     let popularMovies = Movie.popularMovies()
     let nowPlayingMovies = Movie.nowPlayingMovies()
@@ -24,38 +24,8 @@ class FeaturedViewController: UIViewController, UICollectionViewDataSource {
         nowPlayingCollectionView.dataSource = self
     }
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if collectionView == popularCollectionView {
-            return popularMovies.count
-        } else if collectionView == nowPlayingCollectionView {
-            return nowPlayingMovies.count
-        } else {
-            return 0
-        }
-    }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        if collectionView == popularCollectionView {
-            if let cell = popularCollectionView.dequeueReusableCell(withReuseIdentifier: "popularCell", for: indexPath) as? PopularCollectionViewCell {
-                cell.title.text = popularMovies[indexPath.item].title
-                cell.image.image = UIImage(named: popularMovies[indexPath.item].backdrop)
-                return cell
-            }
-        } else if collectionView == nowPlayingCollectionView {
-            if let cell = nowPlayingCollectionView.dequeueReusableCell(withReuseIdentifier: "nowPlayingCell", for: indexPath) as? NowPlayingCollectionViewCell {
-                cell.titleLabel.text = nowPlayingMovies[indexPath.item].title
-                cell.dateLabel.text = nowPlayingMovies[indexPath.item].releaseDate
-                cell.imageView.image = UIImage(named: nowPlayingMovies[indexPath.item].poster)
-                return cell
-                
-            }
-            
-        }
-        
-        
-
-    return UICollectionViewCell()
-}
+    
+    
 
 }
