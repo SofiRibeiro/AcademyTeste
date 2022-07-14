c250d3c7c8cb5568bb76654884341c4b1a99e694
SofiRibeiro <sofiasantana168@gmail.com>
2022-07-14 16:08:48 -0300

    Termina tela de detalhes

--- TestandoGit/Controllers/DetailsViewController.swift
+++ TestandoGit/Controllers/DetailsViewController.swift
@@ -11,12 +11,27 @@ class DetailsViewController: UIViewController {
 
     var movie: Movie?
     
+    @IBOutlet var backdropImage: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var poster: UIImageView!
+    @IBOutlet var ratingLabel: UILabel!
+    @IBOutlet var overviewLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        print(movie)
-        self.title = movie?.title
+        
+        guard let movie = movie else {
+            return
+        }
+        self.title = movie.title
+        backdropImage.image = UIImage(named: movie.backdrop)
+        titleLabel.text = movie.title
+        poster.image = UIImage(named: movie.poster)
+        ratingLabel.text = "Rating: \(movie.voteAverage)/10"
+        overviewLabel.text = movie.overview
+        
     }
     
 }
